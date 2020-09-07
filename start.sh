9e46e59fcd54bb65cf26abbc26f3eb9c9614c1e8
Amitav Chakraborty <amchakra@c3r3p5.hive.fi>
2020-09-07 23:01:03 +0300

    messing around with the script

--- start.sh
+++ start.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-if [ ! -d "/home/update_web" ]; then
+#if [ ! -d "/home/update_web" ]; then
     mkdir /home/update_web
-fi
+#fi
 cd /home/update_web
-if [ ! -d "/home/update_web/.git" ]; then
+#if [ ! -d "/home/update_web/.git" ]; then
     git init
-fi
+#fi
 git pull https://github.com/amchakra-hive/roger-skyline.git > temp
-gitcheck=$(cat temp)
-if [ "$gitcheck" == "Already up to date." ]; then
-    rm temp
-    exit
+#gitcheck=$(cat temp)
+#if [ "$gitcheck" == "Already up to date." ]; then
+#    rm temp
+#    exit
 
-else
-    rm temp
-    rm -rf /var/www/html_old
-    mv  /var/www/html /var/www/html_old
+#else
+#    rm temp
+#    rm -rf /var/www/html_old
+#    mv  /var/www/html /var/www/html_old
     cp -R /home/update_web/ /var/www/html/
-    logfile='/home/deploy_script.log'
-    echo -e "-------------------------------" >> $logfile
-    echo -e "\nThe time of the last update:\nTime: $(date "+ %Y-%m-%d  %H:%M:%S")\n" >> $logfile
-fi
-
+#    logfile='/home/deploy_script.log'
+#    echo -e "-------------------------------" >> $logfile
+#    echo -e "\nThe time of the last update:\nTime: $(date "+ %Y-%m-%d  %H:%M:%S")\n" >> $logfile
+#fi
+echo "ServerName \"http://192.168.99.100/\"" >> /etc/apache2/apache2.conf
 service apache2 start
