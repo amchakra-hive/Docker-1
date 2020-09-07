9a4ca2c8df88e19d0bdf327afa30b983240faa97
Amitav Chakraborty <amchakra@c3r3p5.hive.fi>
2020-09-07 23:10:53 +0300

    fixed the web directory issue

--- start.sh
+++ start.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-#if [ ! -d "/home/update_web" ]; then
-    mkdir /home/update_web
-#fi
+mkdir /home/update_web
 cd /home/update_web
-#if [ ! -d "/home/update_web/.git" ]; then
-    git init
-#fi
-git pull https://github.com/amchakra-hive/roger-skyline.git > temp
-#gitcheck=$(cat temp)
-#if [ "$gitcheck" == "Already up to date." ]; then
-#    rm temp
-#    exit
-
-#else
-#    rm temp
-#    rm -rf /var/www/html_old
-#    mv  /var/www/html /var/www/html_old
-    cp -R /home/update_web/ /var/www/html/
-#    logfile='/home/deploy_script.log'
-#    echo -e "-------------------------------" >> $logfile
-#    echo -e "\nThe time of the last update:\nTime: $(date "+ %Y-%m-%d  %H:%M:%S")\n" >> $logfile
-#fi
+git init
+git pull https://github.com/amchakra-hive/roger-skyline.git 
+rm -rf  /var/www/html 
+cp -R /home/update_web/ /var/www/html/
 echo "ServerName \"http://192.168.99.100/\"" >> /etc/apache2/apache2.conf
 service apache2 start
